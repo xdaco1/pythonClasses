5e291caeda1aeb5827ef1b8f67686b5c41e29c05
Daniel Calderon Ortegon <daniel.calderon.ortegon.contractor@bbva.com>
2020-12-09 08:40:02 -0500

    object modeling

---
+++ Platzi/54.objectModeling.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+def startProgress(title):
+    global progress_x
+    sys.stdout.write(title + ": [" + "-"*40 + "]" + chr(8)*41)
+    sys.stdout.flush()
+    progress_x = 0
+
+def progress(x):
+    global progress_x
+    x = int(x * 40 // 100)
+    sys.stdout.write("#" * (x - progress_x))
+    sys.stdout.flush()
+    progress_x = x
+
+def endProgress():
+    sys.stdout.write("#" * (40 - progress_x) + "]\n")
+    sys.stdout.flush()
+
+if __name__ == "__main__":
+    startProgress("Título progreso")
+    progress(20)
+    progress(40)
+    progress(60)
+    progress(80)
+    progress(100)
+    endProgress()
